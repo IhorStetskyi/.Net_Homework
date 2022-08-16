c5ba1a1277334fc139851c74504f236c3724ead9
Ihor <Ihor_Stetskyi@epam.com>
2022-08-16 18:37:11 +0300

    Hw 15 First commit

---
+++ Stetskyi_Homework_15/Client/Client/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Threading.Tasks;
+
+namespace Client
+{
+    class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            MyClient client = new MyClient();
+            await client.GetInfo();
+            await client.GetSuccess();
+            await client.GetRedirection();
+            await client.GetClientError();
+            await client.GetServerError();
+            await client.GetHeaderName();
+            await client.GetCookieName();
+        }
+    }
+       
+}

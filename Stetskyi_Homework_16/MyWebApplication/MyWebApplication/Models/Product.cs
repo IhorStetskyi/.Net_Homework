b991f0b10c47d6850dcc895bfd529cf16711864b
Ihor <Ihor_Stetskyi@epam.com>
2022-08-19 11:56:22 +0300

    HW 16 Added

---
+++ Stetskyi_Homework_16/MyWebApplication/MyWebApplication/Models/Product.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyWebApplication.Models
+{
+    public class Product
+    {
+        public int ProductID { get; set; }
+        public string ProductName { get; set; }
+        public string CompanyName { get; set; }
+        public string CategoryName { get; set; }
+        public string QuantityPerUnit { get; set; }
+        public decimal UnitPrice { get; set; }
+        public int UnitsInStock { get; set; }
+        public int UnitsOnOrder { get; set; }
+        public int ReorderLevel { get; set; }
+        public int Discontinued { get; set; }
+    }
+}
---
+++ Stetskyi_Homework_16/MyWebApplication/MyWebApplication/StaticFolder/StaticSourceMethods.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyWebApplication.StaticFolder
+{
+    public static class StaticSourceMethods
+    {
+        public static Configuration CreateNewConfig()
+        {
+            Configuration config;
+            config = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+            return config;
+        }
+        public static void ClearConfigurationManagerConnectionStrings(this Configuration config)
+        {
+            foreach (ConnectionStringSettings section in config.ConnectionStrings.ConnectionStrings)
+            {
+                config.ConnectionStrings.ConnectionStrings.Remove(section.Name);
+            }
+            config.Save();
+        }
+        public static void ProtectConnectionData(this Configuration config, bool protect)
+        {
+            ConnectionStringsSection css = config.GetSection("connectionStrings") as ConnectionStringsSection;
+            switch (protect)
+            {
+                case true:
+                    css.SectionInformation.ProtectSection("DataProtectionConfigurationProvider");
+                    config.Save();
+                    break;
+                case false:
+                    css.SectionInformation.UnprotectSection();
+                    config.Save();
+                    break;
+            }
+        }
+        public static void AddConnectionSettingsToConfig(this Configuration config, string dataSource, string initialCatalog, string connectionSettingsName)
+        {
+            SqlConnectionStringBuilder stringBuilder = new SqlConnectionStringBuilder();
+            ConnectionStringSettings connectionSettings = new ConnectionStringSettings();
+
+            stringBuilder.DataSource = dataSource;
+            stringBuilder.InitialCatalog = initialCatalog;
+            stringBuilder.IntegratedSecurity = true;
+            stringBuilder.Pooling = true;
+
+            connectionSettings.Name = connectionSettingsName;
+            connectionSettings.ConnectionString = stringBuilder.ConnectionString;
+
+            config.ConnectionStrings.ConnectionStrings.Add(connectionSettings);
+            config.Save();
+        }
+        public static string GetConnectionString(string connectionSettingName)
+        {
+            return ConfigurationManager.ConnectionStrings[connectionSettingName].ConnectionString;
+        }
+    }
+}
